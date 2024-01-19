ef651d69ecaf188101433548f11d32462ef393ca
Matiu52 <matius.andreatna@gmail.com>
2024-01-19 19:46:30 +0700

    create web virtual piano

---
+++ WebVirtualPiano/src/js/index.js
@@ -0,0 +1,13 @@
+/*? no js js needed from me */
+const chords = ["a", "c", "d", "e", "f"]
+const note = document.getElementById("note")
+
+chords.forEach(chord => {
+    const button = document.createElement("button")
+    button.innerText = chord
+    button.addEventListener("click", () => {
+        const sound = new Audio(`./assets/${chord}.wav`)
+        sound.play()
+    })
+    note.appendChild(button)
+})
