d66bf5d8ed9bf86d6505e5d82ae80d2a31caec7a
Matiu52 <matius.andreatna@gmail.com>
2024-01-27 12:47:01 +0700

    create web profil

---
+++ WebProfile/src/js/index.js
@@ -0,0 +1,7 @@
+function redirInstagram() {
+    window.location.href = "https://instagram.com/matiu_52"
+}
+
+function redirWhatsapp() {
+    window.location.href = "https://wa.me/+628123123123"
+}
